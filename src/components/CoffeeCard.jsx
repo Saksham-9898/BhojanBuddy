c4604947644fbcac74236f2a6571bf936e9baa78
Saksham Sharma <saksham9879@gmail.com>
2025-05-07 01:50:30 +0530

    changes in featured restaurants

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './coffeecard.css';
 
-function CoffeeCard({ image, name, description, price }) {
+function CoffeeCard({ image, name, description,  }) {
   return (
     <div className="coffee-card">
       <img src={image} alt={name} className="coffee-image" />
       <div className="coffee-info">
         <h3>{name}</h3>
         <p>{description}</p>
-        <h4>${price}</h4>
-        <button className="order-now-btn">Order Now</button>
+        {/* <h4>${price}</h4> */}
+        <button className="order-now-btn">Book Now</button>
       </div>
     </div>
   );
--- src/components/CoffeeCarousel.jsx
+++ src/components/CoffeeCarousel.jsx
@@ -6,37 +6,36 @@ function CoffeeCarousel() {
   const coffees = [
     {
       image: '/capp.jpg',
-      name: 'Cappuccino',
-      description: 'Coffee 50% | Milk 50%',
-      price: '8.50',
+      name: 'Mannat Haveli',
+      // description: '',
+      // price: '8.50',
     },
     {
       image: '/e52a5566b4db383db9b5071cff5e508f.jpg',
-      name: 'Chai Latte',
-      description: 'Coffee 50% | Milk 50%',
-      price: '8.50',
+      name: 'Neelkanth Star',
+      // description: '',
+      // price: '8.50',
     },
     {
       image: '/d21cd4dac4c920557d0a66dc54217c32.jpg',
-      name: 'Macchiato',
-      description: 'Coffee 50% | Milk 50%',
-      price: '8.50',
+      name: 'Usha Kiran Palace',
+      // description: '',
+      // price: '8.50',
     },
     {
       image: '/a9345bb5570ed0d9da242130d2f04691.jpg',
-      name: 'Expresso',
-      description: 'Coffee 50% | Milk 50%',
-      price: '8.50',
+      name: 'Moti Mahal Delux',
+      // description: '',
+      // price: '8.50',
     },
     
   ];
 
   return (
     <div className="coffee-carousel-container">
-      <h2 className="section-title">Enjoy a new blend of coffee style</h2>
+      <h2 className="section-title">Flavors from Every Corner</h2>
       <p className="section-subtitle">
-        Explore all flavours of coffee with us. There is always a new cup worth experiencing
-      </p>
+      Explore top-rated restaurants offering global tastes—because great food has no borders.      </p>
       <div className="coffee-carousel">
         {coffees.map((coffee, index) => (
           <CoffeeCard
